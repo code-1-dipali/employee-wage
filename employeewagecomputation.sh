fcbedc834151dff17b06e27531e35ab7811cb8b6
ikalpesh <kalpeshmahajan90@gmail.com>
2021-06-03 22:54:36 +0530

    PartTimeEmpAndWages

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,17 +1,23 @@
 echo "welcome to employee wage"
 
-present=1
-absent=0
+isPartTimePresent=2
+isFullTimePresent=1
+isAbsent=0
 wagePerHour=20
 workingHr=0
-empCheck=$((RANDOM%2))
-if [ $empCheck -eq $present ]
+empCheck=$((RANDOM%3))
+if [ $empCheck -eq $isFullTimePresent ]
 then
 	echo "Employee present"
 	workingHr=8
+elif [ $empCheck -eq $isPartTimePresent ]
+then
+	echo "Employee present for Part time"
+	workingHr=4
 else
 	echo "Employee absent"
 	workingHr=0
 fi
 salary=$((wagePerHour*workingHr))
 echo "Daily employee wage : $salary"
+
