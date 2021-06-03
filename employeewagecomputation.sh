0f5bb4f7bec397413a3279abde39c8f24542b606
ikalpesh <kalpeshmahajan90@gmail.com>
2021-06-03 22:48:00 +0530

    Daily wages

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -2,10 +2,16 @@ echo "welcome to employee wage"
 
 present=1
 absent=0
+wagePerHour=20
+workingHr=0
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq $present ]
 then
 	echo "Employee present"
+	workingHr=8
 else
 	echo "Employee absent"
+	workingHr=0
 fi
+salary=$((wagePerHour*workingHr))
+echo "Daily employee wage : $salary"
